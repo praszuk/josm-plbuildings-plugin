b62d44b23377e26c9ab7b8d91993d01b830d2ec3
Paweł Raszuk <pawel.raszuk@gmail.com>
2022-04-26 23:13:53 +0200

    Add undoUtil function util for UndoRedoHandler

---
+++ src/main/java/org/openstreetmap/josm/plugins/plbuildings/Utils.java
@@ -0,0 +1,25 @@
+package org.openstreetmap.josm.plugins.plbuildings;
+
+import org.openstreetmap.josm.command.Command;
+import org.openstreetmap.josm.data.UndoRedoHandler;
+
+public class Utils {
+    /**
+     * Undo all commands until specific command object.
+     * @param handler instance
+     * @param untilCmd command to which it will be undone
+     * @param inclusive undo untilCmd too
+     */
+    public static void undoUntil(UndoRedoHandler handler, Command untilCmd, boolean inclusive) {
+        while (handler.hasUndoCommands()){
+            if (handler.getLastCommand() == untilCmd){
+                if (inclusive){
+                    handler.undo();
+                }
+                break;
+            }
+
+            handler.undo();
+        }
+    }
+}
