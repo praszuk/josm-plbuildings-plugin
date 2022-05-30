5b22ecac27e7fb6fae2f4ca2791938354f612aaf
Paweł Raszuk <pawel.raszuk@gmail.com>
2022-05-31 01:10:36 +0200

    Add testImportBuildingShareTwoNodesWithBuilding

---
+++ test/unit/org/openstreetmap/josm/plugins/plbuildings/ShareNodesWithObjectTest.java
@@ -0,0 +1,50 @@
+package org.openstreetmap.josm.plugins.plbuildings;
+
+import mockit.Mock;
+import mockit.MockUp;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.testutils.JOSMTestRules;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+import static org.openstreetmap.josm.plugins.plbuildings.ImportUtils.importOsmFile;
+
+public class ShareNodesWithObjectTest {
+    @Rule
+    public JOSMTestRules rules = new JOSMTestRules().main();
+
+    static {
+        new MockUp<BuildingsAction>(){
+            @Mock
+            public DataSet getBuildingsAtCurrentLocation(){
+                DataSet importData = importOsmFile(
+                        new File("test/data/share_nodes_with_object/import_building.osm"),
+                        ""
+                );
+                assertNotNull(importData);
+
+                return importOsmFile(new File("test/data/share_nodes_with_object/import_building.osm"), "");
+            }
+        };
+    }
+    @Test
+    public void testImportBuildingShareTwoNodesWithBuilding(){
+        DataSet ds = importOsmFile(new File("test/data/share_nodes_with_object/way_building.osm"), "");
+        assertNotNull(ds);
+
+        BuildingsAction.performBuildingImport(ds);
+
+        Way building = (Way) ds.getWays().toArray()[0]; // doesn't matter which one
+        assertEquals(
+            building.getNodes().stream()
+                .skip(1)
+                .filter(node -> node.isReferredByWays(2))
+                .count(),
+        2);
+    }
+
+}
