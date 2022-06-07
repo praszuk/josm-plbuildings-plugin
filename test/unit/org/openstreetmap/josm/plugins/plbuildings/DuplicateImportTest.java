5db6c53a932765f551d946983d56f04f525c174b
Paweł Raszuk <pawel.raszuk@gmail.com>
2022-06-07 16:16:51 +0200

    Add test SimpleDuplicateCheckNotDuplicate

---
+++ test/unit/org/openstreetmap/josm/plugins/plbuildings/DuplicateImportTest.java
@@ -0,0 +1,45 @@
+package org.openstreetmap.josm.plugins.plbuildings;
+
+import mockit.Mock;
+import mockit.MockUp;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.plugins.plbuildings.actions.BuildingsImportAction;
+import org.openstreetmap.josm.plugins.plbuildings.validators.BuildingsWayValidator;
+import org.openstreetmap.josm.testutils.JOSMTestRules;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+import static org.openstreetmap.josm.plugins.plbuildings.ImportUtils.importOsmFile;
+
+public class DuplicateImportTest {
+    @Rule
+    public JOSMTestRules rules = new JOSMTestRules().main();
+
+    @Test
+    public void testSimpleDuplicateCheckNotDuplicate(){
+        new MockUp<BuildingsImportAction>(){
+            @Mock
+            public DataSet getBuildingsAtCurrentLocation(){
+                return importOsmFile(new File("test/data/duplicate_import/import_building.osm"), "");
+            }
+        };
+
+        DataSet ds = importOsmFile(
+            new File("test/data/duplicate_import/simple_replace_base.osm"),
+            "");
+        assertNotNull(ds);
+
+        Way buildingToReplace = (Way) ds.getWays().stream().filter(way -> way.hasKey("building", "yes")).toArray()[0];
+        ds.setSelected(buildingToReplace);
+
+        BuildingsImportAction.performBuildingImport(ds);
+
+        assertEquals(ds.getWays().stream().filter(BuildingsWayValidator::isBuildingWayValid).count(), 1);
+    }
+
+
+}
