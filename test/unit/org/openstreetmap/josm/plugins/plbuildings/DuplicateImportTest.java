fe52ee386c012e341a02f1c26791d57eb134bc11
Paweł Raszuk <pawel.raszuk@gmail.com>
2022-06-07 16:17:33 +0200

    Add test SimpleDuplicateCheckDuplicateAllNodesEq

--- test/unit/org/openstreetmap/josm/plugins/plbuildings/DuplicateImportTest.java
+++ test/unit/org/openstreetmap/josm/plugins/plbuildings/DuplicateImportTest.java
@@ -41,5 +41,26 @@ public class DuplicateImportTest {
         assertEquals(ds.getWays().stream().filter(BuildingsWayValidator::isBuildingWayValid).count(), 1);
     }
 
+    @Test
+    public void testSimpleDuplicateCheckDuplicateAllNodesEqual(){
+        new MockUp<BuildingsImportAction>(){
+            @Mock
+            public DataSet getBuildingsAtCurrentLocation(){
+                return importOsmFile(new File("test/data/duplicate_import/import_building.osm"), "");
+            }
+        };
+
+        DataSet ds = importOsmFile(
+            new File("test/data/duplicate_import/simple_duplicate_base.osm"),
+            "");
+        assertNotNull(ds);
+
+        Way buildingToReplace = (Way) ds.getWays().stream().filter(way -> way.hasKey("building", "yes")).toArray()[0];
+        ds.setSelected(buildingToReplace);
+
+        BuildingsImportAction.performBuildingImport(ds);
+
+        assertEquals(ds.getWays().stream().filter(way -> way.hasKey("building", "yes")).count(), 1);
+    }
 
 }
