353469451d9116ee739f09c8ef7412e95e00cb84
Paweł Raszuk <pawel.raszuk@gmail.com>
2022-06-26 18:43:30 +0200

    Fix redo case and helper func for tests

--- src/main/java/org/openstreetmap/josm/plugins/plbuildings/commands/UpdateBuildingTagsCommand.java
+++ src/main/java/org/openstreetmap/josm/plugins/plbuildings/commands/UpdateBuildingTagsCommand.java
@@ -2,7 +2,6 @@ package org.openstreetmap.josm.plugins.plbuildings.commands;
 
 import org.openstreetmap.josm.command.Command;
 import org.openstreetmap.josm.command.SequenceCommand;
-import org.openstreetmap.josm.data.UndoRedoHandler;
 import org.openstreetmap.josm.data.osm.*;
 import org.openstreetmap.josm.gui.conflict.tags.CombinePrimitiveResolverDialog;
 import org.openstreetmap.josm.tools.Logging;
@@ -60,22 +59,24 @@ public class UpdateBuildingTagsCommand extends Command {
 
     @Override
     public boolean executeCommand() {
-        List<Command> commands;
-        try {
-            commands = prepareUpdateTagsCommands();
-        } catch (UserCancelException exception) {
-            Logging.debug(
-                "No building tags (id: {0}) update, caused: Cancel conflict dialog by user",
-                selectedBuilding.getId()
-            );
-            return false;
+        if (this.updateTagsCommand == null) {
+            List<Command> commands;
+            try {
+                commands = prepareUpdateTagsCommands(selectedBuilding, newBuilding);
+            } catch (UserCancelException exception) {
+                Logging.debug(
+                    "No building tags (id: {0}) update, caused: Cancel conflict dialog by user",
+                    selectedBuilding.getId()
+                );
+                return false;
+            }
+            if (commands.isEmpty()) {
+                Logging.debug("No tags difference! Canceling!");
+                return true;
+            }
+            this.updateTagsCommand = new SequenceCommand(DESCRIPTION_TEXT, commands);
         }
-        if (commands.isEmpty()){
-            Logging.debug("Duplicated building geometry and tags! Canceling!");
-            return false;
-        }
-        this.updateTagsCommand = new SequenceCommand(DESCRIPTION_TEXT, commands);
-        UndoRedoHandler.getInstance().add(this.updateTagsCommand);
+        this.updateTagsCommand.executeCommand();
 
         return true;
     }
@@ -86,7 +87,10 @@ public class UpdateBuildingTagsCommand extends Command {
      * @return list of commands as updating tags
      * @throws UserCancelException if user close the window or reject possible tags conflict
      */
-    private List<Command> prepareUpdateTagsCommands() throws UserCancelException {
+    protected List<Command> prepareUpdateTagsCommands(
+        Way selectedBuilding,
+        Way newBuilding
+    ) throws UserCancelException {
         Collection<OsmPrimitive> primitives = Arrays.asList(selectedBuilding, newBuilding);
 
         return CombinePrimitiveResolverDialog.launchIfNecessary(
