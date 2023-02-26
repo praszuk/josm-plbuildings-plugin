0a29f35870e1f27116e2d1027cafc6d6832923ea
Paweł Raszuk <pawel.raszuk@gmail.com>
2023-02-26 15:13:53 +0100

    Add tests for multiple ds but one is empty

--- test/unit/org/openstreetmap/josm/plugins/plbuildings/ImportDataCombineNearestTest.java
+++ test/unit/org/openstreetmap/josm/plugins/plbuildings/ImportDataCombineNearestTest.java
@@ -1,5 +1,7 @@
 package org.openstreetmap.josm.plugins.plbuildings;
 
+import mockit.Mock;
+import mockit.MockUp;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -7,6 +9,8 @@ import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.plugins.plbuildings.data.CombineNearestStrategy;
+import org.openstreetmap.josm.plugins.plbuildings.io.DataSourceProfileDownloader;
 import org.openstreetmap.josm.plugins.plbuildings.models.BuildingsImportData;
 import org.openstreetmap.josm.plugins.plbuildings.models.DataSourceProfile;
 import org.openstreetmap.josm.plugins.plbuildings.models.DataSourceServer;
@@ -89,4 +93,122 @@ public class ImportDataCombineNearestTest {
         assertNotNull(nearestBuilding);
         assertEquals(expectedBuilding, nearestBuilding);
     }
+
+    @Test
+    public void testTwoDSEmptyBothDS(){
+        BuildingsImportManager manager = new BuildingsImportManager(null,null, null);
+        manager.setImportedData(new BuildingsImportData(
+            profileTwoDS.getGeometry(), emptyDS,
+            profileTwoDS.getTags(), emptyDS
+        ));
+        manager.setDataSourceProfile(profileTwoDS);
+
+        Way nearestBuilding = manager.getNearestBuildingFromImportData();
+
+        assertNull(nearestBuilding);
+    }
+
+    @Test
+    public void testTwoDSEmptyOneDSStrategyAcceptOneDS(){
+        Way expectedBuilding = oneBuildingDS.getWays().iterator().next();
+
+        BuildingsImportManager manager = new BuildingsImportManager(null,null, null);
+        manager.setImportedData(new BuildingsImportData(
+            profileTwoDS.getGeometry(), emptyDS,
+            profileTwoDS.getTags(), oneBuildingDS
+        ));
+        manager.setDataSourceProfile(profileTwoDS);
+        BuildingsSettings.COMBINE_NEAREST_BUILDING_ONE_DS_STRATEGY.put(
+            CombineNearestStrategy.ACCEPT_GEOMETRY.toString()
+        );
+
+        Way nearestBuilding = manager.getNearestBuildingFromImportData();
+
+        assertEquals(nearestBuilding, expectedBuilding);
+    }
+
+    @Test
+    public void testTwoDSEmptyOneDSStrategyAcceptOneDSMultipleBuilding(){
+        Way[] buildings = multipleBuildingDS.getWays().toArray(Way[]::new);
+
+        Way expectedBuilding = buildings[2]; // avoid first building to check "nearest" instead of id or something else
+        Node buildingNode = expectedBuilding.getNodes().iterator().next();
+        LatLon latLon = new LatLon(buildingNode.lat(), buildingNode.lon());
+
+        BuildingsImportManager manager = new BuildingsImportManager(null, latLon, null);
+        manager.setImportedData(new BuildingsImportData(
+            profileTwoDS.getGeometry(), emptyDS,
+            profileTwoDS.getTags(), multipleBuildingDS
+        ));
+        manager.setDataSourceProfile(profileTwoDS);
+        BuildingsSettings.COMBINE_NEAREST_BUILDING_ONE_DS_STRATEGY.put(
+            CombineNearestStrategy.ACCEPT_GEOMETRY.toString()
+        );
+
+        Way nearestBuilding = manager.getNearestBuildingFromImportData();
+
+        assertTrue(multipleBuildingDS.getWays().size() > 2);
+        assertNotNull(nearestBuilding);
+        assertEquals(expectedBuilding, nearestBuilding);
+    }
+
+    @Test
+    public void testTwoDSEmptyOneDSStrategyCancelOneDS(){
+        BuildingsImportManager manager = new BuildingsImportManager(null,null, null);
+        manager.setImportedData(new BuildingsImportData(
+            profileTwoDS.getGeometry(), emptyDS,
+            profileTwoDS.getTags(), oneBuildingDS
+        ));
+        manager.setDataSourceProfile(profileTwoDS);
+        BuildingsSettings.COMBINE_NEAREST_BUILDING_ONE_DS_STRATEGY.put(CombineNearestStrategy.CANCEL.toString());
+
+        Way nearestBuilding = manager.getNearestBuildingFromImportData();
+
+        assertNull(nearestBuilding);
+    }
+
+    @Test
+    public void testTwoDSEmptyOneDSStrategyAskUserAcceptOneDS(){
+        Way expectedBuilding = oneBuildingDS.getWays().iterator().next();
+
+        BuildingsImportManager manager = new BuildingsImportManager(null,null, null);
+        manager.setImportedData(new BuildingsImportData(
+            profileTwoDS.getGeometry(), emptyDS,
+            profileTwoDS.getTags(), oneBuildingDS
+        ));
+        manager.setDataSourceProfile(profileTwoDS);
+        BuildingsSettings.COMBINE_NEAREST_BUILDING_ONE_DS_STRATEGY.put(CombineNearestStrategy.ASK_USER.toString());
+
+        new MockUp<DataSourceProfileDownloader>(){
+            @Mock
+            public CombineNearestStrategy askUserToUseOneDS(){
+                return CombineNearestStrategy.ACCEPT_GEOMETRY;
+            }
+        };
+        Way nearestBuilding = manager.getNearestBuildingFromImportData();
+
+        assertEquals(nearestBuilding, expectedBuilding);
+    }
+
+    @Test
+    public void testTwoDSEmptyOneDSStrategyAskUserCancelOneDS(){
+        BuildingsImportManager manager = new BuildingsImportManager(null,null, null);
+        manager.setImportedData(new BuildingsImportData(
+            profileTwoDS.getGeometry(), emptyDS,
+            profileTwoDS.getTags(), oneBuildingDS
+        ));
+        manager.setDataSourceProfile(profileTwoDS);
+        BuildingsSettings.COMBINE_NEAREST_BUILDING_ONE_DS_STRATEGY.put(CombineNearestStrategy.ASK_USER.toString());
+
+        new MockUp<DataSourceProfileDownloader>(){
+            @Mock
+            public CombineNearestStrategy askUserToUseOneDS(){
+                return CombineNearestStrategy.CANCEL;
+            }
+        };
+        Way nearestBuilding = manager.getNearestBuildingFromImportData();
+
+        assertNull(nearestBuilding);
+    }
+
 }
