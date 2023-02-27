fe971feb182458980043c8de40936b95988673f4
Paweł Raszuk <pawel.raszuk@gmail.com>
2023-02-27 17:49:22 +0100

    Move cloneBuilding to external util class

---
+++ src/main/java/org/openstreetmap/josm/plugins/plbuildings/utils/CloneBuilding.java
@@ -0,0 +1,23 @@
+package org.openstreetmap.josm.plugins.plbuildings.utils;
+
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Way;
+
+public class CloneBuilding {
+    /**
+     *
+     * @param building – to clone
+     * @return cloned building – tags, nodes and id are new. if building is null, then returns null
+     */
+    public static OsmPrimitive cloneBuilding(OsmPrimitive building){
+        if (building == null){
+            return null;
+        }
+        Way newBuilding = new Way();
+        ((Way)(building)).getNodes().forEach(n -> newBuilding.addNode(new Node(n.getCoor())));
+        building.getKeys().forEach(newBuilding::put);
+
+        return newBuilding;
+    }
+}
