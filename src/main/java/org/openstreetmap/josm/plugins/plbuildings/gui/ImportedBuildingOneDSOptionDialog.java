bed2fe4098fb2d6a3b49c11a826429665982328d
Paweł Raszuk <pawel.raszuk@gmail.com>
2023-02-27 13:43:02 +0100

    Add gui dialog for Combining conflict (1 ds)

---
+++ src/main/java/org/openstreetmap/josm/plugins/plbuildings/gui/ImportedBuildingOneDSOptionDialog.java
@@ -0,0 +1,30 @@
+package org.openstreetmap.josm.plugins.plbuildings.gui;
+
+import javax.swing.*;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+/**
+ * It shows when user need to decide what strategy to use if only one data source has building data.
+ */
+public class ImportedBuildingOneDSOptionDialog {
+    /**
+     * Shows confirmation dialog
+     * @return true if user clicks to use available data source, else false ("no" button/canceled)
+     */
+    public static boolean show(String availableDatasource){
+        Object[] CHOICES = {tr("Use only") + " : " + availableDatasource, tr("Cancel")};
+
+        int result = JOptionPane.showOptionDialog(
+            null,
+            tr("Incomplete data sources. Do you want to use only one of them to import building ?"),
+            tr("Building import confirmation"),
+            JOptionPane.YES_NO_OPTION,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            CHOICES,
+            CHOICES[0]
+        );
+        return result == JOptionPane.YES_OPTION;
+    }
+}
