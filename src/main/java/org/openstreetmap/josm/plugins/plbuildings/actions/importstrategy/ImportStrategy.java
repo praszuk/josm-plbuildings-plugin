ffa5de8d9b5b8e6fac7ef13ad188ac4f3c05817a
Paweł Raszuk <pawel.raszuk@gmail.com>
2024-08-22 01:48:42 +0200

    Add final

--- src/main/java/org/openstreetmap/josm/plugins/plbuildings/actions/importstrategy/ImportStrategy.java
+++ src/main/java/org/openstreetmap/josm/plugins/plbuildings/actions/importstrategy/ImportStrategy.java
@@ -20,10 +20,10 @@ import org.openstreetmap.josm.plugins.plbuildings.utils.NearestBuilding;
 import org.openstreetmap.josm.tools.Logging;
 
 public abstract class ImportStrategy {
-    protected BuildingsImportManager manager;
-    protected BuildingsImportStats importStats;
-    protected DataSet currentDataSet;
-    protected Way importedBuilding;
+    protected final BuildingsImportManager manager;
+    protected final BuildingsImportStats importStats;
+    protected final DataSet currentDataSet;
+    protected final Way importedBuilding;
 
     public ImportStrategy(BuildingsImportManager manager, BuildingsImportStats importStats, Way importedBuilding) {
         this.manager = manager;
