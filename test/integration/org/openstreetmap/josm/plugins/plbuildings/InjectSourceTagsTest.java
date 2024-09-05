ad87e54c8b2c31f6c6a2f1d0f82fef17d0866775
Paweł Raszuk <pawel.raszuk@gmail.com>
2024-09-06 01:45:10 +0200

    Add edge-cases tests for missing imported data

--- test/integration/org/openstreetmap/josm/plugins/plbuildings/InjectSourceTagsTest.java
+++ test/integration/org/openstreetmap/josm/plugins/plbuildings/InjectSourceTagsTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.plugins.plbuildings.enums.CombineNearestStrategy;
 import org.openstreetmap.josm.plugins.plbuildings.models.BuildingsImportData;
 import org.openstreetmap.josm.plugins.plbuildings.models.DataSourceProfile;
 import org.openstreetmap.josm.plugins.plbuildings.models.DataSourceServer;
@@ -54,5 +55,53 @@ public class InjectSourceTagsTest {
         Way resultBuilding = currentDataSet.getWays().stream().findFirst().orElseThrow();
         Assertions.assertEquals(resultBuilding.get("source:building"), profile.getTags());
         Assertions.assertEquals(resultBuilding.get("source:geometry"), profile.getGeometry());
+    }
+
+    @Test
+    void testTwoDsImportButTagsSourceMissing() {
+        DataSourceServer server = new DataSourceServer("server", "127.0.0.1");
+        DataSourceProfile profile = new DataSourceProfile(server.getName(), "ds_geom", "ds_tags", "profile");
+
+        DataSet importDataSet = importOsmFile(new File("test/data/simple_building.osm"), "");
+        Assertions.assertFalse(importDataSet.getWays().stream().findFirst().orElseThrow().hasTag("source:building"));
+        Assertions.assertFalse(importDataSet.getWays().stream().findFirst().orElseThrow().hasTag("source:geometry"));
+
+        BuildingsSettings.COMBINE_NEAREST_BUILDING_ONE_DS_STRATEGY.put(
+            CombineNearestStrategy.ACCEPT_GEOMETRY.toString()
+        );
+
+        DataSet currentDataSet = new DataSet();
+
+        BuildingsImportManager manager = new BuildingsImportManager(currentDataSet, null, null);
+        manager.setImportedData(new BuildingsImportData("ds_geom", importDataSet, "ds_tags", new DataSet()));
+        manager.setCurrentProfile(profile);
+        manager.processDownloadedData();
+
+        Way resultBuilding = currentDataSet.getWays().stream().findFirst().orElseThrow();
+        Assertions.assertEquals(resultBuilding.get("source:building"), profile.getGeometry());
+        Assertions.assertFalse(resultBuilding.hasTag("source:geometry"));
+    }
+
+    @Test
+    void testTwoDsImportButGeometrySourceMissing() {
+        DataSourceServer server = new DataSourceServer("server", "127.0.0.1");
+        DataSourceProfile profile = new DataSourceProfile(server.getName(), "ds_geom", "ds_tags", "profile");
+
+        DataSet importDataSet = importOsmFile(new File("test/data/simple_building.osm"), "");
+        Assertions.assertFalse(importDataSet.getWays().stream().findFirst().orElseThrow().hasTag("source:building"));
+        Assertions.assertFalse(importDataSet.getWays().stream().findFirst().orElseThrow().hasTag("source:geometry"));
+
+        BuildingsSettings.COMBINE_NEAREST_BUILDING_ONE_DS_STRATEGY.put(CombineNearestStrategy.ACCEPT.toString());
+
+        DataSet currentDataSet = new DataSet();
+
+        BuildingsImportManager manager = new BuildingsImportManager(currentDataSet, null, null);
+        manager.setImportedData(new BuildingsImportData("ds_geom", new DataSet(), "ds_tags", importDataSet));
+        manager.setCurrentProfile(profile);
+        manager.processDownloadedData();
+
+        Way resultBuilding = currentDataSet.getWays().stream().findFirst().orElseThrow();
+        Assertions.assertEquals(resultBuilding.get("source:building"), profile.getTags());
+        Assertions.assertFalse(resultBuilding.hasTag("source:geometry"));
     }
 }
